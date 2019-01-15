8374352f2d566d91850c87356a72ef4cd4347c23
Juan <yu2008juan1@hotmail.com>
2019-01-15 22:15:52 +0100

    1

--- src/main/java/fr/insa/sosa/RestProject/MyActuators.java
+++ src/main/java/fr/insa/sosa/RestProject/MyActuators.java
@@ -1,22 +1,64 @@
 package fr.insa.sosa.RestProject;
 
+import java.util.List;
+
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 
-@Path("myactuators/{id}")
+@Path("actuatorservices")
 public class MyActuators {
 	Room room = new Room();
+	private static final String SUCCESS_RESULT="success";
+	private static final String FAILURE_RESULT="failure";
+	// get list of id of actuators
+	// http://localhost:8888/RestProject/webapi/actuatorservices/myactuators
+	/*@GET
+	@Path("/myactuators")
+    @Produces(MediaType.TEXT_PLAIN)
+	public List<String> getAcuators() {
+		return room.getListAct(); 
+    }*/
 	
+	// get actuator with id
+	// http://localhost:8888/RestProject/webapi/actuatorservices/myactuators/id
 	@GET
+	@Path("/myactuators/{id}")
     @Produces(MediaType.TEXT_PLAIN)
 	public boolean getState(@PathParam("id")String id) {
 		// get state of the actuator
-		return room.getAct(id).getState();  
-		//return false;
+		return room.getAct(id).getState(); 
+    }
+	
+	// update actuator with id
+	// http://localhost:8888/RestProject/webapi/actuatorservices/myactuators/id
+	@PUT
+	@Path("/myactuators/{id}")
+    @Produces(MediaType.TEXT_PLAIN)
+	public String changeState(@PathParam("id")String id) {
+		// get state of the actuator
+		room.getAct(id).changeState();  
+		return SUCCESS_RESULT;
+    }
+		
+	// insert actuator with id
+	// http://localhost:8888/RestProject/webapi/actuatorservices/myactuators/id
+	
+	
+	//delete actuator with id
+	// http://localhost:8888/RestProject/webapi/actuatorservices/myactuators/id
+	@DELETE
+	@Path("/myactuators/{id}")
+    @Produces(MediaType.TEXT_PLAIN)
+	public String deleteAct(@PathParam("id")String id) {
+		// get state of the actuator
+		//room.getAct(id).changeState();  
+		return SUCCESS_RESULT;
     }
 	
 }
--- src/main/java/fr/insa/sosa/RestProject/RoomTester.java
+++ src/main/java/fr/insa/sosa/RestProject/RoomTester.java
@@ -1,5 +1,5 @@
 package fr.insa.sosa.RestProject;
 
-public class Window {
+public class RoomTester {
 
 }
